61e94c81d3c06db507150c31d1a192611f3cf7db
Nicolas Grenoux <ngrenoux@2D3.42angouleme.fr>
2022-11-21 16:34:42 +0100

    premier push

---
+++ includes/minishell.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngrenoux <ngrenoux@student.42angouleme.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/21 13:56:15 by ngrenoux          #+#    #+#             */
+/*   Updated: 2022/11/21 15:20:27 by ngrenoux         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+#endif
