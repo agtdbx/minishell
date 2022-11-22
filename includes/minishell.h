0c448753aebc4fb68ac6aebc1be8c58f96f817c7
Auguste Deroubaix <aderouba@2D1.42angouleme.fr>
2022-11-22 14:35:10 +0100

    Fusion des tous les .h en un seul. Ajout de la première itération du parsing. Ne gère pas les variables d'environement, ni les pipes, ni les redirecyion, ni le here_doc et n'execute pas de commandes

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: aderouba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 13:56:15 by ngrenoux          #+#    #+#             */
-/*   Updated: 2022/11/21 17:25:14 by aderouba         ###   ########.fr       */
+/*   Updated: 2022/11/22 14:33:21 by aderouba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,33 @@
 
 # include "../libft/libft.h"
 
-# include "signal.h"
-# include "parsing.h"
+/*===================================================================
+								PARSING
+===================================================================*/
+
+typedef struct s_command
+{
+	char	*name;
+	char	*input;
+	char	**arg;
+	int		fd_in;
+	int		fd_out;
+}	t_command;
+
+/* ============================parsing.c========================== */
+char		**get_paths(char **envp);
+t_command	*add_command(t_command *commands, t_command command);
+t_command	*parse_buf(char *buf, char **paths);
+void		free_commands(t_command *commands);
+
+/* ============================command.c========================== */
+void		free_command(t_command *command);
+t_command	empty_command(void);
+t_command	get_command(char *input, char **envp);
+void		print_command(t_command *command);
+
+/* =========================command_utils.c======================= */
+char		*get_command_path(char *name, char **paths);
+char		**get_arg(char **split_res, char **paths);
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,16 +6,21 @@
 /*   By: aderouba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 13:50:47 by ngrenoux          #+#    #+#             */
-/*   Updated: 2022/11/21 17:26:10 by aderouba         ###   ########.fr       */
+/*   Updated: 2022/11/22 13:54:55 by aderouba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int	main(void)
+int	main(int argc, char **argv, char **envp)
 {
-	char	*buf;
+	char		*buf;
+	char		**paths;
+	t_command	*commands;
 
+	(void)argc;
+	(void)argv;
+	paths = get_paths(envp);
 	while (1)
 	{
 		buf = readline("> ");
@@ -26,7 +31,10 @@ int	main(void)
 		}
 		if (ft_strcmp(buf, "exit") == 0)
 			break ;
+		commands = parse_buf(buf, paths);
+		free_commands(commands);
 		free(buf);
 	}
+	ft_lstr_free(paths);
 	return (0);
 }
