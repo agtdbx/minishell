7abfaf7bed3fdcf1b9e57f227625eba206ef945a
Nicolas Grenoux <ngrenoux@2F3.42angouleme.fr>
2022-11-30 09:33:49 +0100

    dseF

--- srcs/main.c
+++ srcs/main.c
@@ -3,36 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aderouba <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: ngrenoux <ngrenoux@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 13:50:47 by ngrenoux          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2022/11/30 09:20:01 by ngrenoux         ###   ########.fr       */
-=======
-/*   Updated: 2022/11/29 16:46:58 by aderouba         ###   ########.fr       */
->>>>>>> 1d7a62cc9e6416b15e9537b8601440cbdc44fa18
+/*   Updated: 2022/11/30 09:33:37 by ngrenoux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
-
-char	*concatenate(char *dest, char *src)
-{
-	int i;
-	int j;
-
-	i = 0;
-	while (dest[i] != '\0')
-		i++;
-	j = 0;
-	while (src[j] != '\0')
-	{
-		dest[i + j] = src[j];
-		j++;
-	}
-	dest[i + j] = '\0';
-	return (dest);
-}
 
 void	exit_minishell(char *buf, char **paths, t_list *env)
 {
