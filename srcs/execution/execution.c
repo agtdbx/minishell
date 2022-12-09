de068cb5e7c93c95a31d0aec858834c333ee3216
Auguste Deroubaix <aderouba@2D4.42angouleme.fr>
2022-12-09 11:31:57 +0100

    J'ai plus qu'a normer

--- srcs/execution/execution.c
+++ srcs/execution/execution.c
@@ -6,7 +6,7 @@
 /*   By: aderouba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 10:33:10 by aderouba          #+#    #+#             */
-/*   Updated: 2022/12/09 11:10:15 by aderouba         ###   ########.fr       */
+/*   Updated: 2022/12/09 11:30:08 by aderouba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,7 +64,6 @@ int	execute_cmd(t_data *data, t_cmd *cmds, int i, int **pipes)
 		{
 			env_tmp = get_tab_env(data->env);
 			execve(cmds[i].arg[0], cmds[i].arg, env_tmp);
-			ft_printf_fd("SET TO 126\n", 2);
 			g_exit_status = 126;
 			ft_printf_fd("Error: execution error\n", 2);
 			ft_lstr_free(env_tmp);
@@ -108,6 +107,7 @@ void	interprete_cmds(t_data *data, t_cmd *cmds)
 	int	pipe1[2];
 	int	pipe2[2];
 	int	*pipes[2];
+	int	status;
 
 	if (!cmds || !cmds[0].input || data->pipe_error != -1)
 		return ;
@@ -131,9 +131,10 @@ void	interprete_cmds(t_data *data, t_cmd *cmds)
 	{
 		data->pids[i] = -2;
 		pipe_gestion(cmds, i, pipes[0], pipes[1]);
-		if (data->exit == -1 && cmds[i].name && modify_env(data, &cmds[i]))
+		if (data->exit == -1 && cmds[i].fd_in != -1 && cmds[i].fd_out != -1
+			&& cmds[i].name && modify_env(data, &cmds[i]))
 			close_fds(&cmds[i], NULL, 0);
-		else
+		else if (cmds[i].fd_in != -1 && cmds[i].fd_out != -1)
 			data->pids[i] = execute_cmd(data, cmds, i, pipes);
 		i++;
 	}
@@ -142,25 +143,22 @@ void	interprete_cmds(t_data *data, t_cmd *cmds)
 	while (cmds[i].input != NULL)
 	{
 		if (data->pids[i] == -1)
-		{
 			g_exit_status = 127;
-			ft_printf_fd("SET TO 127\n", 2);
-		}
 		else if (data->pids[i] != -2)
 		{
-			waitpid(data->pids[i], NULL, 0);
-			if (data->exit != -1)
-			{
+			waitpid(data->pids[i], &status, 0);
+			if (status != 0)
+				g_exit_status = 126;
+			else
 				g_exit_status = 0;
-				ft_printf_fd("SET TO 0\n", 2);
-			}
+			if (data->exit != -1)
+				g_exit_status = 0;
 		}
 		i++;
 	}
 	if (data->exit == -1 && (cmds[0].fd_in == -1 || cmds[0].fd_out == -1))
 	{
 		g_exit_status = 1;
-		ft_printf_fd("SET TO 1\n", 2);
 	}
 	free(data->pids);
 }
