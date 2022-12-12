c5761a84bb19e71a4975e656e4cf921f59cdfa96
Auguste Deroubaix <aderouba@2E1.42angouleme.fr>
2022-12-12 13:28:32 +0100

    tkt

--- srcs/utils/error.c
+++ srcs/utils/error.c
@@ -6,7 +6,7 @@
 /*   By: aderouba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 12:47:29 by aderouba          #+#    #+#             */
-/*   Updated: 2022/12/11 18:55:45 by aderouba         ###   ########.fr       */
+/*   Updated: 2022/12/12 13:28:03 by aderouba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,31 +54,62 @@ int	verif_valid_input(char *str, int j)
 {
 	int	only_space;
 
+	if (!str[j] || str[j] == '>' || str[j] == '<')
+		return (1);
+	ft_printf("Start : |%c|\n", str[j]);
 	only_space = 1;
 	while (str[j] && str[j] != '<' && str[j] != '>')
 	{
+		ft_printf("|%c|", str[j]);
 		if (str[j] != ' ' && str[j] != '\t')
 			only_space = 0;
 		j++;
 	}
+	ft_printf("\n");
 	if (only_space)
 	{
 		ft_printf_fd("Error: invalid redirection\n", 2);
-		return (-1);
+		return (0);
 	}
-	return (j);
+	return (1);
+}
+
+int	is_redirection(char *str)
+{
+	int		i;
+	char	quote;
+
+	i = 0;
+	quote = '\0';
+	while (str[i])
+	{
+		quote = quote_gestion(str[i], quote);
+		if (quote == '\0' && (str[i] == '<' || str[i] == '>'))
+			return (1);
+		i++;
+	}
+	return (0);
 }
 
 int	test_bad_redirection(char *str)
 {
-	int	i;
-	int	j;
+	int		i;
+	int		j;
+	char	quote;
 
 	j = 0;
+	quote = '\0';
+	if (!is_redirection(str))
+		return (0);
 	while (str[j])
 	{
-		j = verif_valid_input(str, j);
-		if (j == -1)
+		quote = quote_gestion(str[j], quote);
+		if (quote != '\0')
+		{
+			j++;
+			continue ;
+		}
+		if (!verif_valid_input(str, j))
 			return (1);
 		if (str[j] == '\0')
 			return (0);
@@ -94,6 +125,8 @@ int	test_bad_redirection(char *str)
 		if (test_output_bad_redirection(str, &i, j))
 			return (i);
 		j += i;
+		if (i == 0)
+			j++;
 	}
 	return (0);
 }
